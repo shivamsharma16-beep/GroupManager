269632bf978ff8fce2816a1452f240a92833c4d4
Hydroxy <84828420+shivamsharma16-beep@users.noreply.github.com>
2021-07-12 07:51:19 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -20,11 +20,11 @@ from haruka.modules.helper_funcs.misc import paginate_modules
 from haruka.modules.translations.strings import tld, tld_help 
 from haruka.modules.connection import connected
 
-PM_START = """Hello {}, my name is {}!
+PM_START = """Hello {}, my name is {Miss Delirious}!
 
 You know how hard it is sometimes to manage group so here is the solution for you
 
-I'm group manager bot and Anti-spam for RR Players🃏
+I'm group manager bot and Anti-spam for Team Apocalypse 
 
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
@@ -38,7 +38,7 @@ Made from the source code of [Hydroxy 🇮🇳](t.me/hydroxy_op)
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+ Add me to a group by clicking [here](http://t.me/missdelirious_bot?startgroup=true).
 """
 
 
